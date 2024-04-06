c3aef9cd5663e82e3049c4a1d4d9dd00d5379f01
sacredsliver <sliver_@mail.ru>
2024-04-06 14:53:21 +0300

    Add files via upload

---
+++ dz7/main.py
@@ -0,0 +1,18 @@
+from auchan import *
+import time
+
+def main(keyword):
+    goods_list = scraper.goods(keyword)
+    return goods_list
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print("\033c")
+    keyword = input("Введите ключевое слово для поиска товаров: ")
+    big_list = main(keyword)
+    unique_list = db.unique(big_list)
+    print("\033c")
+    db.csv_file(keyword, big_list)
+    db.json_file(keyword, unique_list)
+    # db.upload("auchan", keyword, main_list) 
+    print(f"Время выполнения: {time.time() - start_time:.2f} секунд")
